1ddcb142c81c6f85dbc79b51fb3519b869c477bd
dmarsh223 <danmarsh223@gmail.com>
2017-04-27 12:28:49 -0400

    added listeners for operation buttons and began perform operation method.

--- app/src/main/java/com/dmstudios/calculator/MainActivity.java
+++ app/src/main/java/com/dmstudios/calculator/MainActivity.java
@@ -64,5 +64,33 @@ public class MainActivity extends AppCompatActivity {
         button8.setOnClickListener(listener);
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
+
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                String op = b.getText().toString();
+                String value = newNumber.getText().toString();
+                if (value.length()!= 0){
+                    performOperation(value,op);
+                }
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+            }
+        };
+
+        buttonEquals.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+        buttonMinus.setOnClickListener(opListener);
+        buttonPlus.setOnClickListener(opListener);
     }
+
+    private void performOperation(String value, String operation){
+        if (null == operand1){
+            operand1 = Double.valueOf(value);
+        }
+    }
+
+
 }
